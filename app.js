fd40bf5cd31c5f360b293e582e9e1801405581d7
Atka555 <agata-kwiek@hotmail.com>
2018-04-13 20:48:02 +0200

    Added working converter and tests

---
+++ app.js
@@ -0,0 +1,86 @@
+let romanMap = {
+  'M': 1000,
+  'D': 500,
+  'C': 100,
+  'L': 50,
+  'X': 10,
+  'V': 5,
+  'I': 1,
+}
+
+let changeMap = {
+  'IIII': 'IV',
+  'VIV':  'IX',
+  'XXXX': 'XL',
+  'LXL':  'XC',
+  'CCCC': 'CD',
+  'DCD':  'CM',
+}
+
+function main(){
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '>> ',
+  });
+
+  rl.prompt();
+
+  rl.on('line', (line) => {
+    switch (line.trim()) {
+      case 'exit':
+        process.exit(0);
+        break;
+      default:
+        let ret = toRoman(line);
+        console.log(ret);
+        break;
+    }
+    rl.prompt();
+  }).on('close', () => {
+    process.exit(0);
+  });
+}
+
+function toRoman(number){
+  let romanNumber = '';
+  while (number > 0) {
+    for (let roman in romanMap) {
+      let value = romanMap[roman];
+      if (!(number % value)) {
+        romanNumber += roman;
+        number -= value;
+        break;
+      }
+    }
+  }
+  return replaceBad(romanNumber.split("").reverse().join(""));
+}
+
+function validateNumber(input){
+  return /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+    .test(input);
+}
+
+function replaceBad(inputRoman){
+  while(!validateNumber(inputRoman)){
+    let outputRoman = '';
+    for (let roman in changeMap) {
+      let rep = changeMap[roman];
+      let regex = new RegExp(roman);
+      if (inputRoman.includes(roman)) {
+        outputRoman = inputRoman.replace(regex, rep)
+      }
+    }
+    inputRoman = outputRoman;
+  }
+  return inputRoman;
+}
+
+main();
+
+module.exports = {
+  toRoman,
+  validateNumber,
+  replaceBad,
+}
---
+++ test/test.js
@@ -0,0 +1,73 @@
+const {assert} = require('chai');
+const {toRoman, validateNumber, replaceBad} = require('../app');
+
+describe('App', function(){
+  describe('toRoman', function() {
+    var tests = [
+      {args: 1,    expected: 'I'},
+      {args: 2,    expected: 'II'},
+      {args: 3,    expected: 'III'},
+      {args: 4,    expected: 'IV'},,
+      {args: 5,    expected: 'V'},
+      {args: 9,    expected: 'IX'},
+      {args: 10,    expected: 'X'},
+      {args: 20,   expected: 'XX'},
+      {args: 30,   expected: 'XXX'},
+      {args: 40,   expected: 'XL'},
+      {args: 50,   expected: 'L'},
+      {args: 90,   expected: 'XC'},
+      {args: 100,  expected: 'C'},
+      {args: 200,  expected: 'CC'},
+      {args: 300,  expected: 'CCC'},
+      {args: 400,  expected: 'CD'},
+      {args: 500,  expected: 'D'},
+      {args: 900,  expected: 'CM'},
+      {args: 1000, expected: 'M'},
+      {args: 2000, expected: 'MM'},
+      {args: 3000, expected: 'MMM'},
+    ];
+
+    tests.forEach(function(test) {
+      it('from ' + test.args, function() {
+        var result = toRoman(test.args);
+        assert.equal(result, test.expected);
+      });
+    });
+  });
+
+  describe('validateNumber', function() {
+    var tests = [
+      {args: ['CMXCIX', 'MCDXLIV', 'MMDCLXVI', 'MMMCXI','CCXXII', 'CCCXXXIII'],
+       expected: true},
+      {args: ['CCCCXXII', 'MMMMCCCXXII', 'CCCXXXXII', 'CCCXXIIII', 'CCCMXXII'],
+       expected: false},
+    ];
+
+    tests.forEach(function(test) {
+      for(let i = 0; i < test.args.length; i++){
+        it('validate number', function() {
+          var result = validateNumber(test.args[i]);
+          assert.equal(result, test.expected);
+        });
+      }
+    });
+  });
+
+  describe('replaceBad', function() {
+    var tests = [
+      {args: 'IIII',    expected: 'IV'},
+      {args: 'VIV',     expected: 'IX'},
+      {args: 'XXXX',    expected: 'XL'},
+      {args: 'LXL',     expected: 'XC'},
+      {args: 'CCCC',    expected: 'CD'},
+      {args: 'DCD',     expected: 'CM'},
+    ];
+
+    tests.forEach(function(test) {
+      it('from ' + test.args, function() {
+        var result = replaceBad(test.args);
+        assert.equal(result, test.expected);
+      });
+    });
+  });
+});
